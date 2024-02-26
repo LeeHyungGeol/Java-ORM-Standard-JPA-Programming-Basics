44cc527db7c88c0db0456efb8127ef93782a4335
LeeHyungGeol <gudrjf1138@gmail.com>
2024-02-26 22:33:16 +0900

    feat(값-타입): 기본값 타입

---
+++ src/main/java/hellojpa/PrimitiveMain.java
@@ -0,0 +1,11 @@
+package hellojpa;
+
+public class PrimitiveMain {
+  public static void main(String[] args) {
+    Integer a = 10;
+    Integer b = a;
+
+    System.out.println("a = " + a);
+    System.out.println("b = " + b);
+  }
+}
